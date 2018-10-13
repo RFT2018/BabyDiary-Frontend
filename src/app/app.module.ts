3a063ef338e09d535a3b345db5fa644ae60baad4
noway0032@gmail.com <noway0032@gmail.com>
2018-10-13 21:00:14 +0200

    timeline stat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { NavComponent } from './nav/nav.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    NavComponent,
+    TimelineComponent
   ],
   imports: [
     BrowserModule
