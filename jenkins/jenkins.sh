58adf4605ac076fd49eefaa1d3edce368a7b8547
noway0032@gmail.com <noway0032@gmail.com>
2018-10-21 20:01:11 +0200

    jenkins.sh 9

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -4,6 +4,6 @@ sudo mkdir -p /docker-babydiary-frontend-temp
 sudo cd /docker-babydiary-frontend-temp
 sudo docker build -f jenkins/Dockerfile -t 'noway0032/babydiary-frontend:v2' .
 sudo cp -r * /docker-babydiary-frontend-temp
+sudo rm -rf *
 sudo docker rm -f 'babydiary-frontend'
 sudo docker run -d --rm --name 'babydiary-frontend' -v /docker-babydiary-frontend-temp:/usr/src/app -p 80:4200 'noway0032/babydiary-frontend:v2' npm start
-sudo docker rmi -f 'noway0032/babydiary-frontend:v2'
