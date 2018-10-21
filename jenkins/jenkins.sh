253b6605d86a33068ba05f60046f478040b72a22
noway0032@gmail.com <noway0032@gmail.com>
2018-10-21 20:21:23 +0200

    jenkins.sh 13

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -7,4 +7,3 @@ sudo cp -r * /docker-babydiary-frontend-temp
 sudo rm -rf *
 sudo docker rm -f 'babydiary-frontend'
 sudo docker run -d --rm --name babydiary-frontend -v /docker-babydiary-frontend-temp:/usr/src/app -p 80:4200 'noway0032/babydiary-frontend:latest' npm start
-sudo docker rmi -f 'noway0032/babydiary-frontend:latest'
