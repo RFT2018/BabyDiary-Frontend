6bd9a2222829408eb99771bccd16a0f68215c9e4
noway0032@gmail.com <noway0032@gmail.com>
2018-10-21 22:09:11 +0200

    jenkins.sh 19

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -1,10 +1,17 @@
 sudo docker rmi -f 'noway0032/babydiary-frontend:latest'
-
+echo -e "---\n\t'noway0032/babydiary-frontend:latest' torles \n---"
 sudo rm -rf /docker-babydiary-frontend-temp
 sudo mkdir -p /docker-babydiary-frontend-temp
+echo -e "---\n\tkönyvtar letrehozasa \n---"
 sudo cd /docker-babydiary-frontend-temp
 sudo docker build -f jenkins/Dockerfile -t 'noway0032/babydiary-frontend:latest' .
+echo -e "---\n\t'noway0032/babydiary-frontend:latest' build lesz \n---"
 sudo cp -r * /docker-babydiary-frontend-temp
 sudo rm -rf *
+echo -e "---\n\tadatmozgatas \n---"
 sudo docker rm -f 'babydiary-frontend'
+echo -e "---\n\t'babydiary-frontend' rorolve \n---"
 sudo docker run -d --name babydiary-frontend -v /docker-babydiary-frontend-temp:/usr/src/app -p 80:4200 'noway0032/babydiary-frontend:latest'
+echo -e "---\n\t'babydiary-frontend' run \n---"
+sudo docker rmi -f 'noway0032/babydiary-frontend:latest'
+echo -e "---\n\t'noway0032/babydiary-frontend:latest' torles \n---"
