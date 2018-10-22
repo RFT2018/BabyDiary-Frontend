e8eb6680207de1c3dd85402e9eb5aaffa9a6f688
noway0032@gmail.com <noway0032@gmail.com>
2018-10-22 02:12:12 +0200

    jenkins.sh 20

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -1,17 +1,34 @@
-sudo docker rmi -f 'noway0032/babydiary-frontend:latest'
-echo -e "---\n\t'noway0032/babydiary-frontend:latest' torles \n---"
-sudo rm -rf /docker-babydiary-frontend-temp
-sudo mkdir -p /docker-babydiary-frontend-temp
-echo -e "---\n\tkönyvtar letrehozasa \n---"
-sudo cd /docker-babydiary-frontend-temp
-sudo docker build -f jenkins/Dockerfile -t 'noway0032/babydiary-frontend:latest' .
-echo -e "---\n\t'noway0032/babydiary-frontend:latest' build lesz \n---"
-sudo cp -r * /docker-babydiary-frontend-temp
+#!/usr/bin/env bash
+!/bin/bash
+
+if [ "$1" != "" ]; then
+        PROJECT_NAME="$1"
+else
+        PROJECT_NAME="pabydiary-frontend"
+fi
+if [ "$2" != "" ]; then
+        PARAM="$2"
+else
+        PARAM=""
+fi
+sudo df -h
+
+echo -e "---\t A projekt neve: $PROJECT_NAME \t---\n"
+
+sudo docker rmi -f noway0032/$PROJECT_NAME:latest
+sudo docker rm -f repo-$PROJECT_NAME
+echo -e "---\t A korabbi '$PROJECT_NAME' project torolve! \t---\n"
+
+#sudo rm -rf /docker-$PROJECT_NAME-temp
+#sudo mkdir -p /docker-$PROJECT_NAME-temp
+#echo -e "---\t A '/docker-$PROJECT_NAME-temp' konyvtar elkeszult \t---\n"
+
+sudo docker build -f jenkins/Dockerfile -t noway0032/$PROJECT_NAME:latest .
+echo -e "---\t A '/docker-$PROJECT_NAME-temp' konyvtar elkeszult \t---\n"
+
+#sudo cp -r * /docker-$PROJECT_NAME-temp
+sudo docker run -it --name repo-$PROJECT_NAME -v /docker-$PROJECT_NAME-temp:/usr/src/app -p 80:4200 noway0032/$PROJECT_NAME:latest $PARAM
+
+#sudo docker rmi -f noway0032/$PROJECT_NAME:latest
 sudo rm -rf *
-echo -e "---\n\tadatmozgatas \n---"
-sudo docker rm -f 'babydiary-frontend'
-echo -e "---\n\t'babydiary-frontend' rorolve \n---"
-sudo docker run -d --name babydiary-frontend -v /docker-babydiary-frontend-temp:/usr/src/app -p 80:4200 'noway0032/babydiary-frontend:latest'
-echo -e "---\n\t'babydiary-frontend' run \n---"
-sudo docker rmi -f 'noway0032/babydiary-frontend:latest'
-echo -e "---\n\t'noway0032/babydiary-frontend:latest' torles \n---"
+sudo df -h
