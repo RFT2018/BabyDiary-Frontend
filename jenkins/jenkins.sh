6095c8082be399383d7b0eb11ca2b30cf6d1387e
noway0032@gmail.com <noway0032@gmail.com>
2018-10-22 03:16:31 +0200

    jenkins.sh 22

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -12,23 +12,18 @@ else
         PARAM=""
 fi
 sudo df -h
-
+sudo mkdir -p /docker-temp
+sudo mkdir -p /docker-temp/$PROJECT_NAME
 echo -e "---\t A projekt neve: $PROJECT_NAME \t---\n"
 
 sudo docker rmi -f noway0032/$PROJECT_NAME:latest
-sudo docker rm -f repo-$PROJECT_NAME
+sudo docker rm -f $PROJECT_NAME
 echo -e "---\t A korabbi '$PROJECT_NAME' project torolve! \t---\n"
 
-#sudo rm -rf /docker-$PROJECT_NAME-temp
-#sudo mkdir -p /docker-$PROJECT_NAME-temp
-#echo -e "---\t A '/docker-$PROJECT_NAME-temp' konyvtar elkeszult \t---\n"
-
 sudo docker build -f jenkins/Dockerfile -t noway0032/$PROJECT_NAME:latest .
-echo -e "---\t A '/docker-$PROJECT_NAME-temp' konyvtar elkeszult \t---\n"
+echo -e "---\t A '$PROJECT_NAME' konyvtar elkeszult \t---\n"
 
-#sudo cp -r * /docker-$PROJECT_NAME-temp
-sudo docker run -it --name repo-$PROJECT_NAME -v /docker-$PROJECT_NAME-temp:/usr/src/app -p 80:4200 noway0032/$PROJECT_NAME:latest $PARAM
+sudo docker run -d --restart always --live-restore --name $PROJECT_NAME -v /docker/$PROJECT_NAME:/usr/src/app/temp -p 80:4200 noway0032/$PROJECT_NAME:latest $PARAM
 
-#sudo docker rmi -f noway0032/$PROJECT_NAME:latest
 sudo rm -rf *
 sudo df -h
