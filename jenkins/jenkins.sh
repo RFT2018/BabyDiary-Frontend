6e96b6fc1c5a21f825344ea95ebbe45dbec78a3f
noway0032@gmail.com <noway0032@gmail.com>
2018-10-22 03:45:21 +0200

    jenkins.sh 24

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
+echo -e "###\n###\t Script elindult, kezdeti tarhely \n###"
+sudo df -h
 
 if [ "$1" != "" ]; then
         PROJECT_NAME="$1"
 else
         PROJECT_NAME="pabydiary-frontend"
 fi
+echo -e "###\n###\t A projekt neve: $PROJECT_NAME \n###"
 
-sudo df -h
 sudo mkdir -p /docker-temp
 sudo mkdir -p /docker-temp/$PROJECT_NAME
-echo -e "---\t A projekt neve: $PROJECT_NAME \t---\n"
+echo -e "###\n###\t Lonyvtarak letrehozva \n###"
 
 sudo docker rmi -f noway0032/$PROJECT_NAME:latest
-echo -e "---\t A korabbi '$PROJECT_NAME' image torolve! \t---\n"
+echo -e "###\n###\t A korabbi '$PROJECT_NAME' image torolve \n###"
 
 sudo docker build -f jenkins/Dockerfile -t noway0032/$PROJECT_NAME:latest .
-echo -e "---\t Az uj '$PROJECT_NAME' image elkiszitve \t---\n"
+echo -e "###\n###\t Az uj '$PROJECT_NAME' image elkeszitve \n###"
 
 sudo docker rm -f $PROJECT_NAME
-echo -e "---\t A korabbi '$PROJECT_NAME' repo torolve! \t---\n"
+echo -e "###\n###\t A korabbi '$PROJECT_NAME' repo torolve \n###"
 
 sudo docker run -d --restart always --live-restore --name $PROJECT_NAME -v /docker/$PROJECT_NAME:/usr/src/app/temp -p 80:4200 noway0032/$PROJECT_NAME:latest
-echo -e "---\t A uj '$PROJECT_NAME' repo elinditva! \t---\n"
+echo -e "###\n###\t A uj '$PROJECT_NAME' repo elinditva \n###"
+
+sudo docker rmi -f noway0032/$PROJECT_NAME:latest
+echo -e "###\n###\t Az aktuális '$PROJECT_NAME' image torolve \n###"
 
 sudo rm -rf *
+echo -e "###\n###\t GIT mappa torolve, zaro tarhely \n###"
 sudo df -h
