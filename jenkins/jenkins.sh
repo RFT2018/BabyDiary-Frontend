7d2c8cb63ac377a4266217009cde12a7185cc7a9
noway0032@gmail.com <noway0032@gmail.com>
2018-10-22 16:58:34 +0200

    jankins script clean

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
-echo -e "###\n###\t Script elindult, kezdeti tarhely \n###"
+echo -e "\n###\n###\t SH script started, system: \n###"
 sudo df -h
+sudo free -m
 
 if [ "$1" != "" ]; then
         PROJECT_NAME="$1"
 else
         PROJECT_NAME="pabydiary-frontend"
 fi
-echo -e "###\n###\t A projekt neve: $PROJECT_NAME \n###"
+echo -e "\n###\n###\t Name of project: $PROJECT_NAME \n###\n"
 
 sudo mkdir -p /docker-temp
 sudo mkdir -p /docker-temp/$PROJECT_NAME
-echo -e "###\n###\t Lonyvtarak letrehozva \n###"
+echo -e "\n###\n###\t Libraries created \n###\n"
 
 sudo docker rmi -f noway0032/$PROJECT_NAME:latest
-echo -e "###\n###\t A korabbi '$PROJECT_NAME' image torolve \n###"
+sudo docker rmi -f $(docker images --all -q -f "since=resin/raspberrypi3-node")
+echo -e "\n###\n###\t The earlier '$PROJECT_NAME' local image deleted \n###\n"
 
 sudo docker build -f jenkins/Dockerfile -t noway0032/$PROJECT_NAME:latest .
-echo -e "###\n###\t Az uj '$PROJECT_NAME' image elkeszitve \n###"
+echo -e "\n###\n###\t The new '$PROJECT_NAME' local image constructed \n###\n"
 
 sudo docker rm -f $PROJECT_NAME
-echo -e "###\n###\t A korabbi '$PROJECT_NAME' repo torolve \n###"
+echo -e "\n###\n###\t The earlier '$PROJECT_NAME' repository deleted \n###\n"
 
 sudo docker run -d --restart always --name $PROJECT_NAME -v /docker/$PROJECT_NAME:/usr/src/app/temp -p 80:4200 noway0032/$PROJECT_NAME:latest
-echo -e "###\n###\t A uj '$PROJECT_NAME' repo elinditva \n###"
-
-sudo docker rmi -f noway0032/$PROJECT_NAME:latest
-echo -e "###\n###\t Az aktualis '$PROJECT_NAME' image torolve \n###"
+echo -e "\n###\n###\t The new '$PROJECT_NAME' local repo started \n###\n"
 
 sudo rm -rf *
-echo -e "###\n###\t GIT mappa torolve, zaro tarhely \n###"
+echo -e "\n###\n###\t GIT folder deleted, system: \n###"
 sudo df -h
+sudo free -m
