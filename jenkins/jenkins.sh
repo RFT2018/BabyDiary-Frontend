d7ed7e44ba36cce8d685778c3894d74ff3cce67f
noway0032@gmail.com <noway0032@gmail.com>
2018-10-22 03:57:23 +0200

    jenkins.sh 25

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -22,7 +22,7 @@ echo -e "###\n###\t Az uj '$PROJECT_NAME' image elkeszitve \n###"
 sudo docker rm -f $PROJECT_NAME
 echo -e "###\n###\t A korabbi '$PROJECT_NAME' repo torolve \n###"
 
-sudo docker run -d --restart always --live-restore --name $PROJECT_NAME -v /docker/$PROJECT_NAME:/usr/src/app/temp -p 80:4200 noway0032/$PROJECT_NAME:latest
+sudo docker run -d --restart always --name $PROJECT_NAME -v /docker/$PROJECT_NAME:/usr/src/app/temp -p 80:4200 noway0032/$PROJECT_NAME:latest
 echo -e "###\n###\t A uj '$PROJECT_NAME' repo elinditva \n###"
 
 sudo docker rmi -f noway0032/$PROJECT_NAME:latest
